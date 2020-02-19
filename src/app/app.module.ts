f4973955ad0f0778e09cb945a253be837fa4f579
XanderUZZZER <xanderuzzzer@gmail.com>
2020-02-19 21:56:02 +0200

    3 files modified, 3 added, 1 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,13 @@ import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { FormsModule } from "@angular/forms";
 import { HttpClientModule } from "@angular/common/http";
-
-import { RouterModule, Router } from '@angular/router';
-
-import { AppComponent } from "./app.component";
-import { HelloComponent } from "./hello.component";
 import { CookieService } from 'ngx-cookie-service';
 
+import { AppComponent } from "./app.component";
+
 @NgModule({
-  imports: [BrowserModule, FormsModule, HttpClientModule, RouterModule.forRoot(
-  [
-    { path: "", component: AppComponent}
-  ]
-)],
-  declarations: [AppComponent, HelloComponent],
+  imports: [BrowserModule, FormsModule, HttpClientModule],
+  declarations: [AppComponent],
   providers: [ CookieService ],
   bootstrap: [AppComponent]
 })

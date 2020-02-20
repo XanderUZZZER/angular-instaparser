e9d64087f4fbfd1ab12f8a66a3b369d37aecc326
XanderUZZZER <xanderuzzzer@gmail.com>
2020-02-20 10:19:49 +0200

    Cookie expiration time resolved

--- src/app/cookie/cookie.component.ts
+++ src/app/cookie/cookie.component.ts
@@ -8,43 +8,27 @@ import { CookieService } from "ngx-cookie-service";
 })
 export class CookieComponent implements OnInit {
   cookieValue = "UNKNOWN";
-  cookies: {};
-  time = new Date().getTime();
-  time2 = new Date();
+  temp;
 
   constructor(private cookieService: CookieService) {}
 
   ngOnInit() {
-    this.cookieService.set("Test", "Hello World");
-    //this.cookieService.set("Test1", "Hello World1", this.time2.setSeconds(this.time2.getSeconds()+50000));
-    this.cookieValue = this.cookieService.get("Test1");
-    this.cookies = this.cookieService.getAll();
-    console.log(this.cookies)
-    
-    console.log('================================');
+    this.cookieService.set("cookie1", "cookie val 1", .00035);//.0007 = 1 minute
   }
 
   getCookie() {
-    this.cookieValue = this.cookieService.get("Test1");
+    console.log("--------------in get cookie--------------");
+   
+
+    let date = Date.now();
+    console.log(date);
+    let date2 = new Date(date);
+    console.log(date2);
+
+    
+    this.temp = this.cookieService.getAll();
+    console.log(this.temp);
+    this.cookieValue = this.cookieService.get("cookie1");
     console.log(this.cookieValue);
-    let t1 = this.time2;
-console.log('t1 date\t\t\t\t',t1);
-
-    let t2 = t1.valueOf();
-console.log('t1 seconds\t\t\t',t2);
-
-    let t3 = new Date(t2);
-    t3 = new Date(1582151271000);
-    console.log('t1 added seconds\t',t3);
-    
-    let t4 = new Date(t2 + 600000);
-console.log('t4ddddddddddddd\t\t',t4);
-this.cookieService.set("Test1", "Hello World1", t4);
-    // console.log(this.time);
-    // console.log(this.time2);
-    
-    
-     
-    
   }
 }

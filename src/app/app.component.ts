3d3421e7b43ef721402ae27a871ec54b4aac1876
XanderUZZZER <xanderuzzzer@gmail.com>
2020-02-24 22:22:14 +0200

    3 files modified, 1 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,5 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./app.component.css"]
 })
 export class AppComponent implements OnInit {
-  name = 'app component';
   ngOnInit() {}
 }

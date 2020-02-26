0eb9f4c55d4d0fee6a748bc75eb0d21da5b639db
XanderUZZZER <xanderuzzzer@gmail.com>
2020-02-26 21:25:29 +0200

    4 files modified, 1 added, 12 deleted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,57 +7,5 @@ declare var FB: any;
   styleUrls: ["./app.component.css"]
 })
 export class AppComponent implements OnInit {
-  ngOnInit() {
-    (window as any).fbAsyncInit = function() {
-      FB.init({
-        appId: "192282568679644",
-        cookie: true,
-        xfbml: true,
-        version: "v6.0"
-      });
-      FB.AppEvents.logPageView();
-    };
-
-    (function(d, s, id) {
-      var js,
-        fjs = d.getElementsByTagName(s)[0];
-      if (d.getElementById(id)) {
-        return;
-      }
-      js = d.createElement(s);
-      js.id = id;
-      js.src = "https://connect.facebook.net/en_US/sdk.js";
-      fjs.parentNode.insertBefore(js, fjs);
-    })(document, "script", "facebook-jssdk");
-  }
-
-  submitLogin() {
-    console.log("submit login to facebook");
-    // FB.login();
-    FB.login(response => {
-      console.log("submitLogin", response);
-      if (response.authResponse) {
-        //login success
-        //login success code here
-        //redirect to home page
-      } else {
-        console.log("User login failed");
-      }
-    });
-  }
-
-  submitLogout() {
-    console.log("submit logout of facebook");
-    // FB.login();
-    FB.logout(response => {
-      console.log("submitLogout", response);
-      if (response.authResponse) {
-        //login success
-        //login success code here
-        //redirect to home page
-      } else {
-        console.log("User logout failed");
-      }
-    });
-  }
+  ngOnInit() {}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,25 +2,13 @@ import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { FormsModule } from "@angular/forms";
 import { HttpClientModule } from "@angular/common/http";
-import { CookieService } from "ngx-cookie-service";
-import { RouterModule, Routes } from "@angular/router";
 
 
 import { AppComponent } from "./app.component";
-import { InstaBasicComponent } from "./insta-basic/insta-basic.component";
-
-import { User } from "./insta-basic/user";
-
-const appRoutes: Routes =[
-    { path: '', component: AppComponent},
-    { path: 'about', component: AppComponent},
-    { path: '**', component: AppComponent }
-];
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, HttpClientModule, RouterModule.forRoot(appRoutes)],
-  declarations: [AppComponent, InstaBasicComponent],
-  providers: [CookieService, User],
-  bootstrap: [AppComponent, InstaBasicComponent]
+  imports: [BrowserModule, FormsModule, HttpClientModule],
+  declarations: [AppComponent],
+  bootstrap: [AppComponent]
 })
 export class AppModule {}

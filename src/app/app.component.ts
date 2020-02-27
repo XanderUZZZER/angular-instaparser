d19a1eb363b8229b6f1f6a7bf4687234c3fd6bc2
XanderUZZZER <xanderuzzzer@gmail.com>
2020-02-27 17:55:42 +0200

    5 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from "@angular/core";
 import { FacebookSdk } from "./instagram-graph-api/facebook-sdk";
-import { InstagramGraphApi } from "./instagram-graph-api/instagram-graph-api";
-declare var FB: any;
 
 @Component({
   selector: "my-app",
@@ -9,68 +7,30 @@ declare var FB: any;
   styleUrls: ["./app.component.css"]
 })
 export class AppComponent implements OnInit {
-  insta: InstagramGraphApi = new InstagramGraphApi();
-  face: FacebookSdk = new FacebookSdk();
+  fb: FacebookSdk = new FacebookSdk();
 
   ngOnInit() {
-    this.loadSdk();
+    this.fb.loadSdk();
   }
 
-  loadSdk() {
-    (window as any).fbAsyncInit = function() {
-      FB.init({
-        appId: "192282568679644",
-        cookie: true,
-        xfbml: true,
-        version: "v6.0"
-      });
+  Login() {
+    this.fb.Login();
+  }
 
-      FB.AppEvents.logPageView();
-    };
+  Logout() {
+    this.fb.Logout();
+  }
 
-    (function(d, s, id) {
-      var js,
-        fjs = d.getElementsByTagName(s)[0];
-      if (d.getElementById(id)) {
-        return;
-      }
-      js = d.createElement(s);
-      js.id = id;
-      js.src = "https://connect.facebook.net/en_US/sdk.js";
-      fjs.parentNode.insertBefore(js, fjs);
-    })(document, "script", "facebook-jssdk");
+  GetStatus() {
+    this.fb.GetStatus();
+  }
 
-    FB.getLoginStatus(function(response) {
-      console.log('getLoginStatus')
-      statusChangeCallback(response);
-    });
+  TestApi(){
+    this.fb.testAPI();
+  }
 
-    function checkLoginState() {
-      console.log('checkLoginState')
-      FB.getLoginStatus(function(response) {
-        statusChangeCallback(response);
-      });
-    }
-    function statusChangeCallback(response) {
-      console.log("statusChangeCallback");
-      console.log(response);
-      // The response object is returned with a status field that lets the
-      // app know the current login status of the person.
-      // Full docs on the response object can be found in the documentation
-      // for FB.getLoginStatus().
-      if (response.status === "connected") {
-        // Logged into your app and Facebook.
-        console.log("Welcome!  Fetching your information.... ");
-        FB.api("/me", function(response) {
-          console.log("Successful login for: " + response.name);
-          document.getElementById("status").innerHTML =
-            "Thanks for logging in, " + response.name + "!";
-        });
-      } else {
-        // The person is not logged into your app or we are unable to tell.
-        document.getElementById("status").innerHTML =
-          "Please log " + "into this app.";
-      }
-    }
+  Info() {
+    console.log("info");
+    this.fb.info();
   }
 }
--- src/app/instagram-graph-api/facebook-sdk.ts
+++ src/app/instagram-graph-api/facebook-sdk.ts
@@ -1,13 +1,22 @@
+declare var FB: any;
+declare var globalFB: any;
+
 export class FacebookSdk {
+  response: any;
+
   loadSdk() {
     (window as any).fbAsyncInit = function() {
       FB.init({
-        appId: "{your-app-id}",
+        appId: "192282568679644",
         cookie: true,
         xfbml: true,
-        version: "{api-version}"
+        version: "v6.0",
+        status: true
       });
-      FB.AppEvents.logPageView();
+      //////??????????????????
+      //FB.AppEvents.logPageView();
+
+      FB.getLoginStatus(response => statusChangeCallback(response));
     };
 
     (function(d, s, id) {
@@ -18,12 +27,80 @@ export class FacebookSdk {
       }
       js = d.createElement(s);
       js.id = id;
-      js.src = "https://connect.facebook.net/en_US/sdk.js";
+      //js.src = "https://connect.facebook.net/en_US/sdk/debug.js"; //debug not mimized
+      js.src = "https://connect.facebook.net/en_US/sdk.js"; // prod version minimized
       fjs.parentNode.insertBefore(js, fjs);
     })(document, "script", "facebook-jssdk");
+
+    function statusChangeCallback(response) {
+      if (response.status === "connected") {
+        console.log("Logged in and authorized");
+      } else if (response.status === "not_authorized") {
+        console.log("Not authorized");
+      } else {
+        console.log("Not logged in");
+      }
+    }
+  }
+
+  Login() {
+    console.log("submit login to facebook");
+    // FB.login();
+    FB.login(response => {
+      console.log("submitLogin", response);
+      this.response = response;
+      console.log("this.response", this.response);
+      if (response.authResponse) {
+        console.log("response.authResponse", response.authResponse);
+      } else {
+        console.log("User login failed");
+      }
+    });
+  }
+
+  Logout() {
+    console.log("submit log out of facebook");
+    FB.logout(response => {
+      this.response = response;
+      console.log("submitLoout", response);
+      if (response.authResponse) {
+        console.log("response.authResponse", response.authResponse);
+        //login success
+        //login success code here
+        //redirect to home page
+      } else {
+        console.log("User logout fails");
+      }
+    });
+  }
+
+  GetStatus() {
+    FB.getLoginStatus(response => this.statusChangeCallback(response));
+  }
+
+  testAPI() {
+    // Testing Graph API after login.  See statusChangeCallback() for when this call is made.
+    console.log("Welcome!  Fetching your information.... ");
+    FB.api("/me", function(response) {
+      console.log("Successful login for: " + response.name);
+      document.getElementById("status").innerHTML =
+        "Thanks for logging in, " + response.name + "!";
+    });
+  }
+
+  statusChangeCallback(response) {
+    this.response = response;
+    if (response.status === "connected") {
+      console.log("Logged in and authorized");
+    } else if (response.status === "not_authorized") {
+      console.log("Not authorized");
+    } else {
+      console.log("Not logged in");
+    }
   }
 
   info() {
-    console.log("SDK");
+    console.log("Facebook SDK");
+    console.log(this.response);
   }
 }
